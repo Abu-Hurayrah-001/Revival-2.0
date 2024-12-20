d4a97b1ab1f6672918b7dc565ca6ce61ba27d045
dedic <sahilakhtar5059@gmail.com>
2024-12-20 22:11:36 +0530

    MongoDB connection - part (a)

---
+++ src/libs/dbConnect.ts
@@ -0,0 +1,27 @@
+// IMPORTS
+import mongoose from "mongoose";
+
+// CONNECT TO MONGO_DB
+type ConnectionObject = {
+    isConnected?: number
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+    if (connection.isConnected) {
+        console.log("You are already connected to the database, dear.");
+        return;
+    };
+
+    try {
+        const db = await mongoose.connect(process.env.MONGO_URI || "");
+        connection.isConnected = db.connections[0].readyState;
+        console.log("Database connected. Launching Skynet in 3,2,1...")
+    } catch (error) {
+        console.log("Database connection failed. Gonnar cry?", error);
+        process.exit(1);
+    };
+};
+
+export default dbConnect();
