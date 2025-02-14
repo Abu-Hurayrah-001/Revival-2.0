104e88d003174819f592368ab3438c2d793feabb
dedic <sahilakhtar5059@gmail.com>
2025-02-14 19:30:42 +0530

    Improved the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,21 +8,29 @@ import { SignInTokenData } from "./app/api/auth/sign-in/route";
 export async function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname;
     let signInTokenData: SignInTokenData | null = null;
-    const ispublicPath = path ==="/login"; // Future me, please don't use "/" here.
     const isApiRoute = path.startsWith("/api");
+    const isGuestOnlyPath = path ==="/login";
+    const isGeneralPath = path === "/" || path === "/login" || path === "/api" || path === "/api/auth/send-login-otp" || path === "/api/auth/sign-in"; // Guest-only paths are also included in general paths
 
     // Get sign-in token if it exists
     if (request.cookies.get("signInToken")?.value) {
         signInTokenData = await getSignInTokenData(request);
     };
 
-    // Prevent logged in users trying from accessing "login" page.
-    if (ispublicPath && signInTokenData?.id) {
-        return NextResponse.redirect(new URL("/me", request.nextUrl));
+    // Prevent logged in users trying from accessing "guest-only" routes.
+    if (isGuestOnlyPath && signInTokenData?.id) {
+        if (isApiRoute) {
+            return NextResponse.json({
+                success: false,
+                message: "This routes is only for logged out users."
+            }, { status: 401 });
+        } else {
+            return NextResponse.redirect(new URL("/me", request.nextUrl));
+        };
     };
 
-    // Prevent non-logged in users from trying to access login protected page.
-    if (!ispublicPath && !signInTokenData?.id) {
+    // Prevent non-logged in users from trying to access login protected routes.
+    if (!isGeneralPath && !signInTokenData?.id) {
         if (isApiRoute) {
             return NextResponse.json({
                 success: false,
@@ -34,7 +42,7 @@ export async function middleware(request: NextRequest) {
     };
 
     // Prevent non-admins from accessing admin routes.
-    if (!ispublicPath && (!signInTokenData?.id || signInTokenData?.isAdmin)) {
+    if (!isGeneralPath && (!signInTokenData?.isAdmin)) {
         if (isApiRoute) {
             return NextResponse.json({
                 success: false,
